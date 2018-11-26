8c5ba396cbb90380712d54711b08a7496e09d0fe
Patrick Sy <patrick.alq.sy@gmail.com>
2018-11-26 21:48:09 +0800

    Interaction System Part 1
    Adds interaction text HUD

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    [SerializeField] public string actionText = "Click to interact";
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionSystem : MonoBehaviour {
+
+    private readonly float maxDistance = 3f;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private GameObject interactable;
+    private Text intText;
+
+	// Use this for initialization
+	void Start () {
+        intText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
+            interactable = hit.collider.gameObject;
+
+            if (interactable.tag == "Interactable")
+            {
+                //Debug.Log("Interactable object - " + interactable.name);
+                intText.enabled = true;
+                intText.text = interactable.GetComponent<Interactable>().actionText;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                }
+            }
+        }
+        else
+        {
+            //Debug.Log("Not hitting anything.");
+
+            intText.enabled = false;
+        }
+    }
+}
